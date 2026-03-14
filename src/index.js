487b940473afe4d4c637afaad679b26aed921576
Dhanika Grover <dhanikagrover08@gmail.com>
2026-03-14 21:12:42 +0530

    mongo db connection

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+connectDB();
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw err;
+  }
+})();
+*/
